ac980698f496297692b3eb043bf20ca48c84b480
chasinglulu <wangkartx@gmail.com>
2024-01-03 15:59:00 +0800

    feat(arm): Adding lambert virtual platform support

---
+++ plat/ax/lmt/include/lmt_def.h
@@ -0,0 +1,47 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (C) 2024, Charleye <wangkart@aliyun.com>
+ * All rights reserved.
+ */
+
+#ifndef LMT_DEF_H
+#define LMT_DEF_H
+
+#define PLATFORM_NAME   "lambert virt platform"
+
+/* Clock configuration */
+#define LMT_OSC24M_CLK_IN_HZ       24000000
+
+/* UART configuration */
+#define LMT_UART0_BAUDRATE         115200
+#define LMT_UART0_CLK_IN_HZ        LMT_OSC24M_CLK_IN_HZ
+
+unsigned int lmt_calc_core_pos(u_register_t mpidr);
+void lmt_console_init(void);
+void plat_lmt_gic_init(void);
+void lmt_pwr_gic_on_finish(void);
+void lmt_pwr_gic_off(void);
+
+/* PMU register offsets for CPU*/
+enum {
+	CPU_CL0_C0_0            = 0x0214,
+	CPU_CL0_C0_1            = 0x0218,
+	CPU_CL0_C1_0            = 0x0220,
+	CPU_CL0_C1_1            = 0x0224,
+	CPU_CL0_C2_0            = 0x022c,
+	CPU_CL0_C2_1            = 0x0230,
+	CPU_CL0_C3_0            = 0x0238,
+	CPU_CL0_C3_1            = 0x023c,
+	CPU_CL1_C0_0            = 0x0240,
+	CPU_CL1_C0_1            = 0x0244,
+	CPU_CL1_C1_0            = 0x0248,
+	CPU_CL1_C1_1            = 0x024c,
+	CPU_CL1_C2_0            = 0x0250,
+	CPU_CL1_C2_1            = 0x0254,
+	CPU_CL1_C3_0            = 0x0258,
+	CPU_CL1_C3_1            = 0x025c,
+};
+
+#define LMT_PMU_BASE        0x23190000UL
+
+#endif /* LMT_DEF_H */
